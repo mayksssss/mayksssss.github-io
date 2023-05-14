f9335896cf358e2b6b85018d1aae5a1901318e10
mayksssss <133565292+mayksssss@users.noreply.github.com>
2023-05-14 16:28:45 +0800

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,35 @@
+function addition()
+        {
+            num1 = document.getElementById("num1").value;
+            num2 = document.getElementById("num2").value;
+            sum = parseInt(num1) + parseInt(num2);
+
+            document.write("The sum is " + sum);
+        }
+
+        function subtraction()
+        {
+            num1 = document.getElementById("num1").value;
+            num2 = document.getElementById("num2").value;
+            sub = parseInt(num1) - parseInt(num2);
+
+            document.write("The sum is " + sub);
+        }
+
+        function multiplication()
+        {
+            num1 = document.getElementById("num1").value;
+            num2 = document.getElementById("num2").value;
+            mul = parseInt(num1) * parseInt(num2);
+
+            document.write("The sum is " + mul);
+        }
+
+        function divition()
+        {
+            num1 = document.getElementById("num1").value;
+            num2 = document.getElementById("num2").value;
+            div = parseInt(num1) / parseInt(num2);
+
+            document.write("The sum is " + div);
+        }
